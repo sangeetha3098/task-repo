9c30d1f8c351b312d54711fe9338f5f9cce2a9f9
sangeetha3098 <sangeethashanmugam3098@gmail.com>
2026-07-23 14:54:29 +0530

    SQL task

--- SQL/Day2/DDL/ddl.sql
+++ SQL/Day2/DDL/ddl.sql
@@ -1,2 +1,54 @@
 CREATE DATABASE CompanyDB;
-SHOW DATABASES
+SHOW DATABASES;
+USE CompanyDB;
+
+CREATE TABLE Employees(
+emp_id int primary key auto_increment,
+emp_name varchar(50),
+department varchar(50),
+salary int,
+city varchar(200),
+created_by varchar(50)  default"Admin",
+created_at date,
+updated_by varchar(50),
+updated_at date
+
+);
+
+DESC Employees;
+
+ALTER TABLE Employees ADD (
+email varchar(200),
+phone int,
+experience varchar(50)
+);
+
+DESC Employees;
+
+ALTER TABLE Employees MODIFY salary bigint , MODIFY phone bigint;
+DESC Employees;
+ALTER TABLE Employees RENAME COLUMN emp_name to employee_name; 
+ALTER TABLE Employees RENAME COLUMN city to location;
+DESC Employees;
+ALTER TABLE Employees DROP COLUMN experience , DROP COLUMN email;
+DESC Employees;
+RENAME TABLE Employees to EmployeeDetails;
+DESC EmployeeDetails;
+
+
+CREATE TABLE Students(
+student_id int primary key auto_increment,
+student_name varchar(50),
+course varchar(100),
+age int,
+city varchar(100)
+);
+
+
+DESC Students;
+ALTER TABLE Students ADD COLUMN mobile int;
+DESC Students;
+TRUNCATE TABLE Students;
+DROP TABLE Students;
+DROP TABLE EmployeeDetails;
+DROP DATABASE companydb;
