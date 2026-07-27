b3b730d0a936f8ba85b695f7f66b6f71438bc754
Sangeetha <sangeethashanmugam3098@gmail.com>
2026-07-27 14:22:25 +0530

    Task

--- SQL/Day3/dml.sql
+++ SQL/Day3/dml.sql
@@ -21,11 +21,10 @@ CREATE TABLE Students (
     city varchar(100)
 );
 INSERT INTO Students (student_id, student_name, course, age, city)
-VALUES
-(101, 'Sangeetha', 'B.Tech IT', 22, 'Chennai'),
+VALUES (101, 'Sangeetha', 'B.Tech IT', 18, 'Chennai'),
 (102, 'Rahul', 'B.Sc Computer Science', 21, 'Bangalore'),
 (103, 'Priya', 'MCA', 23, 'Coimbatore'),
-(104, 'Arun', 'B.E Mechanical', 22, 'Madurai'),
+(104, 'Arun', 'B.E Mechanical', 19, 'Madurai'),
 (105, 'Divya', 'B.Com', 20, 'Salem'),
 (106, 'Karthik', 'BCA', 21, 'Puducherry'),
 (107, 'Meena', 'MBA', 24, 'Chennai'),
@@ -39,3 +38,27 @@ SELECT * FROM Employees;
 DELETE FROM Employees WHERE id=5;
 UPDATE Employees SET salary= salary + 10000 WHERE department="Developer" ;
 SELECT * FROM Employees WHERE department = 'Developer';
+UPDATE Students SET city = "Coimbatore" WHERE city ="Chennai";
+SELECT * FROM Students WHERE city="Coimbatore";
+DELETE FROM Students WHERE age < 20;
+SELECT * FROM Students;
+
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    price DECIMAL(10,2),
+    category VARCHAR(100)
+);
+
+INSERT INTO Products (product_id, product_name, price, category) VALUES (101, 'Laptop', 55000.00, 'Electronics'),(102, 'Mobile', 25000.00, 'Electronics'),(103, 'Headphones', 2000.00, 'Accessories'),(104, 'Office Chair', 7000.00, 'Furniture'),(105, 'Water Bottle', 500.00, 'Home');
+
+
+UPDATE Products
+SET price = 60000.00
+WHERE product_id = 101;
+
+DELETE FROM Products
+WHERE product_id = 105;
+
+SELECT * FROM Products;
+
